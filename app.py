7023a8097cba3ad4e3348852ba269a91a31d7384
matematikoy <lucasmarques.mat@gmail.com>
2024-12-30 19:01:08 +0000

    Comentários alterados.

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import io
 
 # Função para obter o token de autorização e salvar em um arquivo
 def obter_token_autorizacao(usuario, senha):
-    url_login = "https://api.grupohne.com.br/api/login"  # URL de login
+    url_login = "https://api.grupohne.com.br/api/login"
     dados_login = {
         "usuario": usuario,  
         "password": senha     
@@ -25,7 +25,7 @@ def obter_token_autorizacao(usuario, senha):
             data = response.json()
             if "token" in data:
                 token = data["token"]
-                # Salva o token
+
                 with open("token.txt", "w", encoding="utf-8") as token_file:
                     token_file.write(token)
                 return token
@@ -44,7 +44,7 @@ def login(username, password):
     else:
         return False, None
 
-# Função para enviar os livros (endpoint da API)
+# Função para enviar os livros
 def enviar_livros(token, unidade, data_inicial, data_final):
     url = "https://api.grupohne.com.br/api/v1/aluno/enviar-livros?page=1&pageSize=10"
     headers = {
@@ -57,11 +57,11 @@ def enviar_livros(token, unidade, data_inicial, data_final):
             "tipo": "pacsedex",
             "tipo_curso": "",
             "cidade": "",
-            "unidade": unidade,  # Unidade baseada no Centro de Custo
+            "unidade": unidade,
             "status_inscricao": "",
             "material_entrega": {"pac": True, "sedex": True},
-            "data_inicial": data_inicial,  # API espera no formato yyyy-mm-dd
-            "data_final": data_final       # API espera no formato yyyy-mm-dd
+            "data_inicial": data_inicial,
+            "data_final": data_final
         },
         "page": 1,
         "pageSize": 350
@@ -92,18 +92,18 @@ def exportar_correios(token, ids):
         "ids": ids
     }
 
-    # Enviar requisição POST para exportar os correios
     response = requests.post(url, json=payload, headers=headers)
 
     # Verificar se a requisição foi bem-sucedida
+
     if response.status_code == 200:
-        # Obter a data de hoje no formato ddmmaaaa para nomear o arquivo
+        # Obter a data de hoje
         data_hoje = datetime.today().strftime("%d%m%Y")
         
-        # O conteúdo da resposta será em formato CSV
         linhas = response.content.decode('utf-8').splitlines()
         
         if len(linhas) > 1:
+
             # Criar um arquivo CSV na memória
             csv_buffer = io.StringIO()
             
@@ -111,7 +111,6 @@ def exportar_correios(token, ids):
             cabecalho = "SERVICO;DESTINATARIO;CEP;LOGRADOURO;NUMERO;COMPLEMENTO;BAIRRO;EMAIL;;;CPF/CNPJ;VALOR_DECLARADO;;TIPO_OBJETO;;;;;AR;MP;;;OBSERVACAO\n"
             csv_buffer.write(cabecalho)
             
-            # Escrever as linhas do CSV
             for linha in linhas[1:]:
                 csv_buffer.write(linha + "\n")
             
@@ -127,13 +126,14 @@ def exportar_correios(token, ids):
 
 # Função principal que será chamada após o login
 def main():
+
     # Verificar se já existe um token no session_state
     if 'token' not in st.session_state:
-        # Caso o token não esteja no session_state, mostra apenas a tela de login
+
+        # Caso o token não esteja no session_state, mostra a tela de login
         st.title("Login")
         st.subheader("Digite suas credenciais do Sistema Grupo HNe")
 
-        # Limpar a tela, exibir apenas campos de login
         usuario = st.text_input("Usuário")
         senha = st.text_input("Senha", type="password")
         login_button = st.button("Entrar")
@@ -141,21 +141,18 @@ def main():
         if login_button:
             sucesso, token = login(usuario, senha)
             if sucesso:
-                st.session_state.token = token  # Armazenar o token no session_state
+                st.session_state.token = token
                 st.success("Login bem-sucedido!")
-                st.rerun()  # Redefine a página para exibir o conteúdo principal
+                st.rerun()
             else:
                 st.error("Usuário ou senha incorretos.")
                 return
-        return  # Não executa nada mais após o login
+        return
 
 
 
-    # Exibir os filtros (Centro de Custo, Datas) se ainda não foram cancelados
     if 'envio_confirmado' not in st.session_state or not st.session_state.envio_confirmado:
-            # Se o token estiver no session_state, continua a execução normal
         st.title("Filtre os alunos.")
-        # Mostrar o centro de custo (ComboBox)
         centro_custo = st.selectbox("Escolha o Centro de Custo", ["BH", "GO", "ES"])
 
         unidade = []
@@ -166,10 +163,8 @@ def main():
         elif centro_custo == "ES":
             unidade = [3]
 
-        # Solicitar a data inicial e final com os calendários
         st.subheader("Escolha as datas")
 
-        # Criar colunas para exibir as datas lado a lado
         col1, col2 = st.columns(2)
 
         # Data inicial
@@ -180,7 +175,6 @@ def main():
         with col2:
             data_final = st.date_input("Data Final", datetime.today())
 
-        # Exibir as datas selecionadas
         st.write(f"Período do Curso: de {data_inicial.strftime('%d/%m/%Y')} a {data_final.strftime('%d/%m/%Y')}")
 
         # Realizar a requisição de envio de livros
@@ -191,53 +185,45 @@ def main():
                 ids_nomes_cursos_emitidos = enviar_livros(st.session_state.token, unidade, data_inicial.strftime("%Y-%m-%d"), data_final.strftime("%Y-%m-%d"))
 
                 if ids_nomes_cursos_emitidos:
-                    # Armazenar os dados dos alunos no session_state para garantir que estarão disponíveis
                     st.session_state.ids_nomes_cursos_emitidos = ids_nomes_cursos_emitidos
                     
-                    # Perguntar se o usuário quer enviar os IDs para exportação
                     confirmar_envio = st.radio(f"Deseja confirmar a exportação de {len(ids_nomes_cursos_emitidos)} alunos?", ['Sim', 'Não'])
 
                     if confirmar_envio == 'Sim':
-                        st.session_state.envio_confirmado = True  # Marcar que o envio foi confirmado
-                        st.rerun()  # Recarregar a tela para exibir o expander
+                        st.session_state.envio_confirmado = True  # Marca que o envio foi confirmado
+                        st.rerun()
                 else:
                     st.warning("Nenhum aluno encontrado para exportação.")
     
-    # Exibir o expander se o envio for confirmado
     if 'envio_confirmado' in st.session_state and st.session_state.envio_confirmado:
         st.title("Detalhes dos alunos a serem exportados")
 
-        # Criar um expander com rolagem
         with st.expander("Lista de alunos a exportar", expanded=True):
             # Obter os dados dos alunos armazenados no session_state
             ids_nomes_cursos_emitidos = st.session_state.get('ids_nomes_cursos_emitidos', [])
 
-            # Definir o conteúdo HTML para a rolagem
             html_content = """
             <div style="max-height: 300px; overflow-y: auto; padding: 10px; border: 1px solid #ddd; margin-bottom: 10px;">
             """
 
-            # Adicionar os alunos ao conteúdo HTML com rolagem
             for item in ids_nomes_cursos_emitidos:
-                nome, curso = item[1], item[2]  # Desempacotar os dados de nome e curso
+                nome, curso = item[1], item[2]
                 html_content += f"<p style='background-color: #d8d4dc; padding: 10px; border-radius: 5px; border: 1px solid #d8d4dc;'><b>Aluno:</b> {nome}<br><b>Curso:</b> {curso}</p>"
 
-            # Fechar a tag <div> e adicionar ao Streamlit
             html_content += "</div>"
 
-            # Exibir o conteúdo com rolagem dentro do expander
             st.markdown(html_content, unsafe_allow_html=True)
 
-        # Criar colunas para os botões de Exportar e Cancelar
         col1, col2 = st.columns([1, 5])
         with col1:
+
             # Verificar se o botão "EXPORTAR" foi pressionado
             if st.button("EXPORTAR"):
                 # Chamar a função de exportação para obter o conteúdo CSV e o nome do arquivo
                 csv_data, file_name = exportar_correios(st.session_state.token, [item[0] for item in ids_nomes_cursos_emitidos])  # Passar os IDs dos alunos
 
                 if csv_data:
-                    # Criar o botão de download diretamente
+
                     st.download_button(
                         label="BAIXAR AQUIVO CSV",
                         data=csv_data,
@@ -249,10 +235,9 @@ def main():
                 
         with col2:
             if st.button("CANCELAR"):
-                st.session_state.envio_confirmado = False  # Voltar para a tela de filtros
-                st.rerun()  # Recarregar a tela para exibir novamente o filtro
+                st.session_state.envio_confirmado = False
+                st.rerun()
                 
 
-# Rodar o aplicativo Streamlit
 if __name__ == "__main__":
     main()
